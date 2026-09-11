6df9176aa558758fd2813e3b8d70a22dc67620c9
TravLaneDesigns <trav.lane115@gmail.com>
2026-09-11 03:14:49 -0400

    Add central data exports

---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+export { STATES } from "./states";
+export { LANDMARKS, LANDMARK_CATEGORIES } from "./landmarks";
+export { STATE_MILESTONES, CARTOGRAPHER_PENCIL, STATE_STAMP_REWARD, getNextStateMilestone } from "./stateMilestones";
+export { STICKER_CATALOG, STICKER_RARITIES, VEHICLE_STICKERS, ANIMAL_STICKERS, SILLY_STICKERS, MYTHIC_STICKERS, STICKER_SETS, STICKER_TRADE_VALUES } from "./stickerCatalog";
+export { SECRET_ACHIEVEMENTS } from "./secretAchievements";

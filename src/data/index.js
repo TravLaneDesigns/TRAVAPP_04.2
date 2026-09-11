50e3e8c98f7a51219257752acc7ed73ca4ec2e94
TravLaneDesigns <trav.lane115@gmail.com>
2026-09-11 03:18:35 -0400

    Export complete passport progression helpers

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,29 @@
-export { STATES } from "./states";
-export { LANDMARKS, LANDMARK_CATEGORIES } from "./landmarks";
-export { STATE_MILESTONES, CARTOGRAPHER_PENCIL, STATE_STAMP_REWARD, getNextStateMilestone } from "./stateMilestones";
-export { STICKER_CATALOG, STICKER_RARITIES, VEHICLE_STICKERS, ANIMAL_STICKERS, SILLY_STICKERS, MYTHIC_STICKERS, STICKER_SETS, STICKER_TRADE_VALUES } from "./stickerCatalog";
-export { SECRET_ACHIEVEMENTS } from "./secretAchievements";
-export { PASSPORT_REGIONS, ATLANTIC_COAST_STATES, PACIFIC_COAST_STATES, LOWER_48_STATES, isRegionComplete, hasCoastToCoast } from "./passportRegions";
+export { STATES } from "./states.js";
+export { LANDMARKS, LANDMARK_CATEGORIES } from "./landmarks.js";
+export {
+  STATE_MILESTONES,
+  CARTOGRAPHER_PENCIL,
+  STATE_STAMP_REWARD,
+  getNextStateMilestone,
+  getCompletedStateMilestones,
+  getCartographerPencilTier,
+} from "./stateMilestones.js";
+export {
+  STICKER_CATALOG,
+  STICKER_RARITIES,
+  VEHICLE_STICKERS,
+  ANIMAL_STICKERS,
+  SILLY_STICKERS,
+  MYTHIC_STICKERS,
+  STICKER_SETS,
+  STICKER_TRADE_VALUES,
+} from "./stickerCatalog.js";
+export { SECRET_ACHIEVEMENTS } from "./secretAchievements.js";
+export {
+  PASSPORT_REGIONS,
+  ATLANTIC_COAST_STATES,
+  PACIFIC_COAST_STATES,
+  LOWER_48_STATES,
+  isRegionComplete,
+  hasCoastToCoast,
+} from "./passportRegions.js";

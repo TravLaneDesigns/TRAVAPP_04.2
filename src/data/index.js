c7c8112d1e26672f631f6618cbe3229f4d56693f
TravLaneDesigns <trav.lane115@gmail.com>
2026-09-11 03:15:50 -0400

    Export passport region helpers

--- src/data/index.js
+++ src/data/index.js
@@ -3,3 +3,4 @@ export { LANDMARKS, LANDMARK_CATEGORIES } from "./landmarks";
 export { STATE_MILESTONES, CARTOGRAPHER_PENCIL, STATE_STAMP_REWARD, getNextStateMilestone } from "./stateMilestones";
 export { STICKER_CATALOG, STICKER_RARITIES, VEHICLE_STICKERS, ANIMAL_STICKERS, SILLY_STICKERS, MYTHIC_STICKERS, STICKER_SETS, STICKER_TRADE_VALUES } from "./stickerCatalog";
 export { SECRET_ACHIEVEMENTS } from "./secretAchievements";
+export { PASSPORT_REGIONS, ATLANTIC_COAST_STATES, PACIFIC_COAST_STATES, LOWER_48_STATES, isRegionComplete, hasCoastToCoast } from "./passportRegions";

f5ba363de61e4f1915abbecd28a62999db03f4e5
TravLaneDesigns <trav.lane115@gmail.com>
2026-09-11 03:23:42 -0400

    Wire V1 data bridge into Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fartV1Bridge } from "./build/fartV1Bridge.js";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [fartV1Bridge(), react()],
   base: "./",
 });
